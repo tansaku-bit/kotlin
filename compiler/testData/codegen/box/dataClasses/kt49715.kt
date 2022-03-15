af6b1d9bd1bcdf30145a24173d07e1fb4c6589c1
Xin Wang <scaventz@outlook.com>
2022-03-15 17:57:53 +0800

    JVM_IR: Handle methods descriptor clash in data class
    1. Extension functions declared in data classes are generated earlier by `generateMembersDeclaredInClassBody`
    2. Extension functions fake override from parent class are generated earlier by `generateFakeOverrideMemberDeclarations`
    
    So it is safe to filter out extension functions inside `generateAdditionalMembersForDataClass`
    
    #KT-49715 Fixed

---
+++ compiler/testData/codegen/box/dataClasses/kt49715.kt
@@ -0,0 +1,21 @@
+// CHECK_BYTECODE_LISTING
+// WITH_STDLIB
+// IGNORE_BACKEND_FIR: JVM_IR
+
+import kotlin.test.*
+
+interface A {
+    fun Any.toString(): String = "hello"
+}
+
+data class B(val x: Int) : A {
+    fun Any.hi() = "hi"
+}
+
+fun box(): String {
+    val b = B(1)
+    assertEquals("B(x=1)", b.toString())
+    assertTrue(b == B(1))
+    assertTrue(1.hashCode() == b.hashCode())
+    return "OK"
+}
--- js/js.tests/tests-gen/org/jetbrains/kotlin/js/test/ir/IrJsCodegenBoxTestGenerated.java
+++ js/js.tests/tests-gen/org/jetbrains/kotlin/js/test/ir/IrJsCodegenBoxTestGenerated.java
@@ -9752,6 +9752,12 @@ public class IrJsCodegenBoxTestGenerated extends AbstractIrJsCodegenBoxTest {
         }
 
         @Test
+        @TestMetadata("kt49715.kt")
+        public void testKt49715() throws Exception {
+            runTest("compiler/testData/codegen/box/dataClasses/kt49715.kt");
+        }
+
+        @Test
         @TestMetadata("mixedParams.kt")
         public void testMixedParams() throws Exception {
             runTest("compiler/testData/codegen/box/dataClasses/mixedParams.kt");
